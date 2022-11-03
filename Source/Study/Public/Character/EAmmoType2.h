8a60d7db280c6f5efa1e1a3870a33fb82a5184c6
Jacob <tldk47@hanmail.net>
2022-11-03 13:41:58 +0900

    enum class AmmoType addded

---
+++ Source/Study/Public/Character/EAmmoType2.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+UENUM(BlueprintType)
+enum class EAmmoType2 : uint8
+{
+	EAT_9mm UMETA(DisplayName = "9mm"),
+	EAT_5mm UMETA(DisplayName = "5.56mm"),
+	EAT_7mm UMETA(DisplayName = "7.62mm"),
+
+	EAT_MAX UMETA(DisplayName = "DefaultMax")
+};
